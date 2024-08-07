1c2845ab4c7785372840931bc6cbc2cd38203d00
TaniaNovikova <tania.novikov@web.de>
2024-08-07 03:05:08 +0200

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,12 @@ import { PagesNavigation } from "pages/EmployeesProject/LayoutEmployeesProject/t
 // import Homework_16 from "homeworks/Homework_16/Homework_16"
 
 // import Lesson_16 from "lessons/Lesson_16/Lesson_16"
+import Lesson_18 from "lessons/Lesson_18/Lesson_18"
 
 function App() {
   return (
     <>
-      <LayoutEmployeesProject>
+      {/* <LayoutEmployeesProject>
         <Routes>
           <Route path={PagesNavigation.HOME}></Route>
           <Route
@@ -23,11 +24,13 @@ function App() {
           <Route path={PagesNavigation.EMPLOYEES} element={<Employees />} />
           <Route path="*" element="Page not found" />
         </Routes>
-      </LayoutEmployeesProject>
+      </LayoutEmployeesProject> */}
 
       {/* <Homework_15 /> */}
       {/* <Homework_16 /> */}
+
       {/* <Lesson_16 /> */}
+      <Lesson_18 />
     </>
   )
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -3,9 +3,10 @@ import { combineSlices, configureStore } from "@reduxjs/toolkit"
 import { counterSlice } from "store/redux/counter/counterSlice"
 import { feedbackSlice } from "./redux/feedback/feedbackSlyces"
 import { employeesSlice } from "./redux/employees/employeesSlice"
+import { catFactSlice } from "./redux/catFact/catFactSlice"
 // `combineSlices` automatically combines the reducers using
 // their `reducerPath`s, therefore we no longer need to call `combineReducers`.
-const rootReducer = combineSlices(counterSlice, feedbackSlice, employeesSlice)
+const rootReducer = combineSlices(counterSlice, feedbackSlice, employeesSlice, catFactSlice)
 // Infer the `RootState` type from the root reducer
 export type RootState = ReturnType<typeof rootReducer>
 
